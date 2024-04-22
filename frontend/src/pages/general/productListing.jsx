0b37fca17eb3e1a1e2462e0b3dc34ad329305797
kxrweng <kwlimlearns@gmail.com>
2024-04-23 02:53:17 +0800

    Done Search

--- frontend/src/pages/general/productListing.jsx
+++ frontend/src/pages/general/productListing.jsx
@@ -1,74 +1,21 @@
-// import React, { useContext, useEffect, useState } from "react";
-// import Customer_Navbar from "../../components/customer_navbar";
-// import { useNavigate } from "react-router-dom";
-// import { GlobalContext } from "../../context";
 
-// const ProductListing = () => {
-//     const { cartItems, setCartItems, productListing, setProductListing } =
-//         useContext(GlobalContext);
-    
-//     const navigation = useNavigate();
-
-//     const handleClick = (product) => {
-//         navigation(`/customer/product/${product.id}`, { state: { product } });
-//     };
-
-    
-
-//     return (
-//         <div>
-//             <Customer_Navbar />
-//             <main className="mt-36">
-//                 <div className="flex flex-row">
-//                     {productListing.map((product) => (
-//                         <div
-//                             key={product.id}
-//                             className="product-card"
-//                             onClick={() => handleClick(product)}
-//                         >
-//                             <img
-//                                 src={product.img}
-//                                 alt={product.name}
-//                                 className="product-image"
-//                             />
-//                             <h3>{product.name}</h3>
-//                             <p>Price: ${product.price}</p>
-//                             <p>Rating:{product.rating}</p>
-//                             <p>Rating: {product.seller}</p>
-//                         </div>
-//                     ))}
-//                 </div>
-//             </main>
-//             <style jsx>{`
-//                 .product-card {
-//                     border: 1px solid #ccc;
-//                     padding: 20px;
-//                     margin: 10px;
-//                     width: 200px; /* Set the width to ensure all cards have the same size */
-//                     text-align: center;
-//                     cursor: pointer; /* Add cursor pointer to indicate clickable */
-//                 }
-
-//                 .product-image {
-//                     max-width: 100%;
-//                     height: auto;
-//                     margin-bottom: 10px;
-//                 }
-//             `}</style>
-//         </div>
-//     );
-// };
-
-// export default ProductListing;
 import React, { useContext, useEffect, useState } from "react";
 import Customer_Navbar from "../../components/customer_navbar";
 import { useNavigate } from "react-router-dom";
 import { GlobalContext } from "../../context";
+import { useLocation } from 'react-router-dom';
+
 
 const ProductListing = () => {
     const { cartItems, setCartItems, productListing, setProductListing } =
         useContext(GlobalContext);
-    
+        const location = useLocation();
+        const { displayedProducts } = location.state;
+
+        useEffect(() => {
+            console.log(displayedProducts);
+        }, [displayedProducts]);
+
     const navigation = useNavigate();
 
     const handleClick = (product) => {
@@ -80,9 +27,23 @@ const ProductListing = () => {
     return (
         <div>
             <Customer_Navbar />
-            <main className="mt-36">
-                <div className="flex flex-row">
-                    {productListing.map((product) => (
+            <main className="mt-36 flex flex-col p-10 gap-10">
+                <section>
+                    <p className = "font-sans font-bold text-[32px]">Trending Products</p>
+                </section>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10">
+                    {displayedProducts.map((product) => {
+                        return(
+                            <section className = "flex flex-col ">
+                                <img src = {product.img} className = "w-[280px] h-[320px] " />
+                                <p className = "font-sans  text-lg text-gray-700">{product.name}</p>
+                                <p className = "font-sans font-bold text-gray-900 ">RM{product.price}</p>
+                            </section>
+                        )
+                    })}
+                    </div>
+                {/* <div className="flex flex-row">
+                    {displayedProducts.map((product) => (
                         <div
                             key={product.id}
                             className="product-card"
@@ -99,24 +60,9 @@ const ProductListing = () => {
                             <p>Rating: {product.seller}</p>
                         </div>
                     ))}
-                </div>
+                </div> */}
             </main>
-            <style jsx>{`
-                .product-card {
-                    border: 1px solid #ccc;
-                    padding: 20px;
-                    margin: 10px;
-                    width: 200px; /* Set the width to ensure all cards have the same size */
-                    text-align: center;
-                    cursor: pointer; /* Add cursor pointer to indicate clickable */
-                }
-
-                .product-image {
-                    max-width: 100%;
-                    height: auto;
-                    margin-bottom: 10px;
-                }
-            `}</style>
+          
         </div>
     );
 };
