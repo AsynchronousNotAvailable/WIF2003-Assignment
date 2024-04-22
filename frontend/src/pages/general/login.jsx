301754cc70edfeabd3dcf6bbf58349d555107f55
junxian <junxiansean8@gmail.com>
2024-04-22 10:10:13 +0800

    add in content

--- frontend/src/pages/general/login.jsx
+++ frontend/src/pages/general/login.jsx
@@ -2,6 +2,13 @@
 import React, { useContext, useState } from "react";
 import { GlobalContext } from "../../context";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const TagLine = styled.h1`
+    font-size: 36px;
+    font-weight: bold;
+    color: #7450DF;
+`
 
 function Login() {
     const [username, setUsername] = useState("");
@@ -21,9 +28,25 @@ function Login() {
         }
     };
 
+    const LoginForm = () => {
+        return (
+            <div>
+                <input
+                    style={{ border: "1px solid black" }}
+                    type="text"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <button onClick={handleLogin}>Login</button>
+            </div>
+        );
+    }
+
     return (
         <div>
-            Login Page
+            <img src="/SyopiLogo.png" alt="logo" />
+            <TagLine>Empowering Campus Commerce: <br/> Connect, Sell, Shop, Thrive! </TagLine>
+            <LoginForm />
             <input
                 style={{ border: "1px solid black" }}
                 type="text"
