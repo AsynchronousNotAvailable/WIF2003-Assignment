27c015ca302cf887e5055c02f9170e4fba2320a8
chenkang02 <91252717+chenkang02@users.noreply.github.com>
2024-04-24 12:50:06 +0800

    add product page ui tidied

--- frontend/src/pages/seller/add_product.jsx
+++ frontend/src/pages/seller/add_product.jsx
@@ -4,7 +4,8 @@ import {useForm} from 'react-hook-form'
 import { useContext } from "react";
 import { GlobalContext } from "../../context";
 import { useNavigate } from "react-router-dom";
-
+import cross_icon from "../../assets/cross_icon.png"
+import add_icon from "../../assets/add_icon_white.png"
 function AddProduct() {
     const { addSellerProduct } = useContext(GlobalContext);
     
@@ -102,91 +103,85 @@ function AddProduct() {
         <>
             <div className="flex ms-5 mb-2 me-5 mt-5">
                 <p className="flex-1 font-bold text-3xl ms-5" type="submit">Add Product</p>
-                <div className="flex">
-                    <img className="mx-2 h-auto max-w-full" src="https://via.placeholder.com/30"></img>
-                    <img className="mx-2 h-auto max-w-full" src="https://via.placeholder.com/30"></img>
-                    <img className="mx-2 mr-0 rounded-lg mx-2 h-auto max-w-full" src="https://via.placeholder.com/30"></img>
-                </div>
-                <br />
             </div>
             <form onSubmit={handleSubmit}>
                 <div className="flex ms-5 me-2 mt-3 w-full">
                 <div className="float-left w-4/5 px-5 py-5">
                     <div className="mb-7 shadow-lg px-5 py-5 rounded-md">
-                        <h2 className="font-w500 mb-2">General Information</h2>
-                        <h3>Product Name</h3>
+                        <h2 className="font-semibold mb-2 text-[#353535] text-2xl">General Information</h2>
+                        <h3 className="font-normal text-[#777980] text-base">Product Name</h3>
                         <input id="product-name" className="mb-2 h-8 w-full border-2 me-4 border-border-grey ps-2 rounded-lg" type="text" placeholder="Type product name here..." value={productName} onChange={handleNameChange}/>
-                        <h3>Description</h3>
+                        <h3 className="font-normal text-[#777980] text-base">Description</h3>
                         <textarea id="product-description" rows={5} className="w-full border-2 me-4 border-border-grey ps-2 rounded-lg" placeholder="Type product description here..."
                         value={productDesc} onChange={handleDescChange}/>
                     </div>
                     <div className="mb-7 shadow-lg px-5 py-2 rounded-md">
-                        <h2 className="font-w500 mb-2">Media</h2>
-                        <h3>Photo</h3>
+                        <h2 className="font-semibold mb-2 text-[#353535] text-2xl mb-2">Media</h2>
+                        <h3 className="font-normal text-[#777980] text-base">Photo</h3>
                         <DragDropImageUploader/>
                     </div>
                     <div className="mb-7 shadow-lg px-5 py-2 rounded-md">
-                        <h2 className="font-w500 mb-2">Pricing</h2>
-                        <h3>Base Price</h3>
+                        <h2 className="font-semibold mb-2 text-[#353535] text-2xl mb-2">Pricing</h2>
+                        <h3 className="font-normal text-[#777980] text-base">Base Price</h3>
                         <input id="product-price" className="mb-2 h-8 w-full border-2 me-4 border-border-grey ps-2 rounded-lg" type="text" placeholder="Type base price here..." 
                         value={productPrice} onChange={handlePriceChange}/>
                         <div className="flex w-full">
                             <div className="float-left w-1/2 mr-5 pb-5">
-                                <h3>Discount Type</h3>
+                                <h3 className="font-normal text-[#777980] text-base">Discount Type</h3>
                                 <select id="discount-type" className=" h-8 w-full border border-border-grey rounded-lg" value={discountType} onChange={handleDiscountTypeChange}>
                                     <option value="none">Select a discount type</option>
                                     <option value="voucher">Voucher</option>
                                 </select>
                             </div>
                             <div className="float-left w-1/2">
-                                <h3>Discount Percentage</h3>
+                                <h3 className="font-normal text-[#777980] text-base">Discount Percentage</h3>
                                 <input id="discount-percentage" className=" h-8 w-full border-2 me-4 border-border-grey ps-2 rounded-lg" type="number" placeholder="Type discount percentage here..."
                                 value={discountPercentage} onChange={handleDiscountPercentageChange} />
                             </div>
                         </div>
                     </div>
                     <div className="mb-7 shadow-lg px-5 py-2 rounded-md">
-                        <h2 className="font-w500 mb-2">Inventory</h2>
+                        <h2 className="font-semibold mb-2 text-[#353535] text-2xl mb-2">Inventory</h2>
                         <div className="flex w-full">
                             <div className="float-left w-1/3 mr-5 pb-5">
-                                <h3>SKU</h3>
+                                <h3 className="font-normal text-[#777980] text-base">SKU</h3>
                                 <input id="sku" type="text" placeholder="Type product SKU here..." className="px-2 h-8 w-full border border-border-grey rounded-lg"
                                 value={sku} onChange={handleSkuChange}/>
                             </div>
                             <div className="float-left w-1/3 mr-5">
-                                <h3>Barcode</h3>
+                                <h3 className="font-normal text-[#777980] text-base">Barcode</h3>
                                 <input id="barcode" type="text" placeholder="Product barcode" className="px-2 h-8 w-full border border-border-grey rounded-lg"
                                 value={barcode} onChange={handleBarcodeChange}/>
                             </div>
                             <div className="float-left w-1/3">
-                                <h3>Quantity</h3>
+                                <h3 className="font-normal text-[#777980] text-base">Quantity</h3>
                                 <input id="quantity" type="text" placeholder="Type product quantity here..." className="px-2 h-8 w-full border border-grey-1100 rounded-lg"
                                 value={quantity} onChange={handleQuantityChange}/>
                             </div>
                         </div>
                     </div>
                     <div className="mb-7 shadow-lg px-5 py-2 rounded-md">
-                        <h2 className="font-w500 mb-2">Shipping</h2>
+                        <h2 className="font-semibold mb-2 text-[#353535] text-2xl mb-2">Shipping</h2>
                         <input className="inline mr-2" type="checkbox" id="physical_product" name="physical_product" value="is_physical_product"/>
                         <span className="text-blue-700">This is a physical product</span>
                         <div className="flex w-full">
                             <div className="float-left w-1/4 mr-5 pb-5">
-                                <h3>Weight</h3>
+                                <h3 className="font-normal text-[#777980] text-base">Weight</h3>
                                 <input id="weight" type="number" placeholder="Product weight..." className="px-2 h-8 w-full border border-border-grey rounded-lg"
                                 value={weight} onChange={handleWeightChange}/>
                             </div>
                             <div className="float-left w-1/4 mr-5">
-                                <h3>Height</h3>
+                                <h3 className="font-normal text-[#777980] text-base">Height</h3>
                                 <input id="height" type="number" placeholder="Height (cm) ..." className="px-2 h-8 w-full border border-border-grey rounded-lg"
                                 value={height} onChange={handleHeightChange}/>
                             </div>
                             <div className="float-left w-1/4 mr-5">
-                                <h3>Length</h3>
+                                <h3 className="font-normal text-[#777980] text-base">Length</h3>
                                 <input id="length" type="number" placeholder="Length (cm)..." className="px-2 h-8 w-full border border-border-grey rounded-lg"
                                 value={length} onChange={handleLengthChange}/>
                             </div>
                             <div className="float-left w-1/4 pb-5">
-                                <h3>Width</h3>
+                                <h3 className="font-normal text-[#777980] text-base">Width</h3>
                                 <input id="width" type="number" placeholder="Width (cm)..." className="px-2 h-8 w-full border border-border-grey rounded-lg"
                                 value={width} onChange={handleWidthChange}/>
                             </div>
@@ -194,13 +189,22 @@ function AddProduct() {
                     </div>
                 </div>
                 <div className="w-1/5 mx-5">
-                    <div className="flex w-full">
-                        <button onClick={handleCancelClick} className="w-1/4 h-8 border-0 bg-blue-400 rounded-lg mx-5 my-3">Cancel</button>
-                        <button className="w-3/4 h-8 border-0 bg-blue-200 rounded-lg mx-5 my-3">Add Product</button>
-                    </div>
+                        <div className="flex w-full">
+                            <button
+                                className="items-center content-center flex w-28 me-2 bg-[#7450DF]/15 rounded-lg h-10"
+                                onClick={handleCancelClick}
+                            >
+                                <span className='flex text-[#7450DF] pl-3'><img className="mt-2 h-3" src={cross_icon}></img><p className='ml-1'>Cancel</p></span>
+                            </button>
+                            <button type="submit"
+                                className="flex-initial w-40 bg-[#7450DF] rounded-lg h-10 mr-5 mb-3"
+                            >
+                                <span className='text-white flex ml-4'><img className="h-4 mt-1" src={add_icon}></img><p className="ml-1">Add Product</p></span>
+                            </button>
+                        </div>
                     <div className="content-center mx-5 mb-7 shadow-lg px-5 py-5 rounded-md">
-                        <h1 className="my-5">Category</h1>
-                        <h3>Product Category</h3>
+                        <h1 className="font-semibold mb-2 text-[#353535] text-2x">Category</h1>
+                        <h3 className="font-normal text-[#777980] text-base">Product Category</h3>
                         <select id="category" className=" h-8 w-full border border-border-grey rounded-lg mb-3"
                         value={category} onChange={handleCategoryChange}>
                             <option value="none">Select a category</option>
@@ -209,14 +213,12 @@ function AddProduct() {
                             <option value="electronics">Electronics</option>
                             <option value="pet">Pet</option>
                         </select>
-                        <h3>Product Tags</h3>
+                        <h3 className="font-normal text-[#777980] text-base">Product Tags</h3>
                         <select id="tags" className=" h-8 w-full border border-border-grey rounded-lg"
                         value={tags} onChange={handleTagsChange}>
                             <option value="none">Select tags</option>
-                            <option value="voucher">Beauty</option>
-                            <option value="voucher">Drinks</option>
-                            <option value="voucher">Tag2</option>
-                            <option value="voucher">Tag3</option>
+                            <option value="fragile">Fragile</option>
+                            <option value="protected">Protected</option>
                         </select>
                     </div>
                 </div>
