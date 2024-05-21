b021eba2f2b729eee978acaf6cbdda78df5be5ad
junxian <junxiansean8@gmail.com>
2024-05-21 12:14:34 +0800

    added shipping address schema in models

--- server/models/customer.js
+++ server/models/customer.js
@@ -2,6 +2,16 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+const addressSchema = new Schema({
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    country: { type: String, required: true },
+    receiverName: { type: String, required: true },
+    receiverPhoneNumber: { type: String, required: true },
+});
+
 const customerSchema = new Schema({
     username: { type: String, required: true, maxLength: 100, unique: true },
     email: { type: String, required: true, maxLength: 100, unique: true },
@@ -11,6 +21,7 @@ const customerSchema = new Schema({
     orders: [{ type: Schema.Types.ObjectId, ref: "Order" }],
     cards: [{ type: Schema.Types.ObjectId, ref: "Card" }],
     cart: { type: Schema.Types.ObjectId, ref: "Cart" },
+    shippingAddress: addressSchema,
 });
 
 exports.CustomerModel = mongoose.model("Customer", customerSchema);
